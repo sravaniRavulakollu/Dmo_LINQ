d769845b374e1223812d7ce74c24d2b0803ef527
sravaniRavulakollu <87975506+sravaniRavulakollu@users.noreply.github.com>
2021-10-17 21:41:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Data.SqlTypes;
+
+namespace Demo_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ Implemetation inc#");
+
+            //Data Source
+            int[] scores = new int[] { 10, 20, 30, 40, 50 };
+            string[] fruits = new string []{ "apple", "bananna", "grapes " };
+
+            //expression
+
+            //IEnumerable <int> ScoreQuery=
+            //from score in scores
+            //  where score > 34
+            //orderby score descending
+            // select score;
+
+            //query
+
+            IEnumerable<string> FruitsQuery =
+                from fruit in fruits
+                where fruit is"bananna" or "grapes"
+                select fruit;
+
+            foreach(var item in FruitsQuery)
+            {
+                Console.WriteLine("Values:{0}\n", item);
+            }
+
+
+
+
+            //foreach (int  item in ScoreQuery)
+            //{
+              //  Console.WriteLine("Values:{0}\n", item);
+            //}
+                       
+        }
+    }
+}
